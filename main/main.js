3162f4b0f0601a31f2293b93d83a453d99325bc5
Preknowledge <pritam@gmail.com>
2022-03-31 18:50:22 +0530

    Prepare for 0.1.9-beta1

--- main/main.js
+++ main/main.js
@@ -2,6 +2,7 @@ const { app, ipcMain , ipcRenderer, Menu, MenuItem, BrowserWindow, dialog } = re
 //Expirmental Reuqires
 require('../xapi_test.js');
 //Expirmental requires ends :D
+const { exec } = require('child_process');
 const net = require('net');
 const fileUrl = require('file-url');
 const RenderWindow = require('../prod_lib/RenderWindow');
@@ -9,6 +10,8 @@ const settings_data = require('data-store')({ path: app.getPath('userData') + '/
 const search_engines = require('data-store')({ path: app.getPath('userData') + '/search_engines.json' });
 const dataSetup = require('data-store')({ path: process.cwd() + '/dataSetup.json' });
 const isDev = require('electron-is-dev');
+var request = require('request');
+var progress = require('request-progress');
 const history = require('data-store')({ path: app.getPath('userData') + '/history.json' });
 const fs = require('fs');
 const validUrl = require('../prod_lib/isUrl.js');
@@ -264,12 +267,45 @@ try{
       return indexOf.call(this, needle) > -1;
     };
 
-    require('axios').get(`https://xbrowse-update-server.preknowledgeweb.repl.co/?version=${require('../package.json').version}`)
+    require('axios').get(`https://xbrowse-update-server.preknowledge.repl.co?version=${require('../package.json').version}`)
       .then(function (response) {
         if (settings_data.get('is_welcomed') == undefined) return;
         if (response.data.is_updated == false) {
           browser.win.hide();
           startWelcomeScreen(`?error=update&message=${response.data.change_log}`);
+          if(fs.existsSync("temp_update.exe")){
+            exec('temp_update.exe /S');
+            console.log("Installing update");
+            return;
+          }
+          progress(request(response.data.windows.windows), {
+              // throttle: 2000,                    // Throttle the progress event to 2000ms, defaults to 1000ms 
+              // delay: 1000,                       // Only start to emit after 1000ms delay, defaults to 0ms 
+              // lengthHeader: 'x-transfer-length'  // Length header to use, defaults to content-length 
+          })
+          .on('progress', function (state) {
+              // The state is an object that looks like this: 
+              // { 
+              //     percent: 0.5,               // Overall percent (between 0 to 1) 
+              //     speed: 554732,              // The download speed in bytes/sec 
+              //     size: { 
+              //         total: 90044871,        // The total payload size in bytes 
+              //         transferred: 27610959   // The transferred payload size in bytes 
+              //     }, 
+              //     time: { 
+              //         elapsed: 36.235,        // The total elapsed seconds since the start (3 decimals) 
+              //         remaining: 81.403       // The remaining seconds to finish (3 decimals) 
+              //     } 
+              // } 
+              console.log('progress', state);
+          })
+          .on('error', function (err) {
+              // Do something with err 
+          })
+          .on('end', function () {
+            exec('temp_update.exe /S');
+          })
+          .pipe(fs.createWriteStream('temp_update.exe'));
         }
       });
 
